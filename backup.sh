d132b2f3c8c6a72bf29c9c9474cc8f7788042e6f
mildamade <milda.dvorak@gmail.com>
2018-07-23 00:06:41 +0200

    oprava backup

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@ export PGPASSWORD="odoo"
 docker exec -i odoocajchandocker_odoo11_1 /bin/bash -c \
 "export PGPASSWORD="odoo" \
 && /usr/bin/pg_dump --host=461f90768059 --dbname=CAJCHAN --username=odoo" \
-> ./cajchan_postgresql.dump
+> /data/odoo_backup_db/cajchan_postgresql.dump
